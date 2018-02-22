68e0ad7cb424e5d342434b90dd23eb6f8942ff21
slie07 <liestevano@gmail.com>
2018-02-22 11:20:01 -0700

    adding more database function

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,10 +1,49 @@
 <?php 
 
+require_once("config.php");
+
+
+
 class Database {
+
+
+		public $connection;
+
+
+		function __construct(){
+
+		$this->open_db_connection();
+
+
+		}
+
 
 		public function open_db_connection(){
 
-		$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+		$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+		if(mysqli_connect_errno()){
+			die("Database connection failed" . mysqli_error());
+
+
+		}
+
+
+}
+
+	public function query($sql) {
+
+		$result = msqli_query($this->connection, $sql);
+		if(!result){
+			die("Query Failed");
+
+		}
+
+		return $result;
+
+
+	}
+
 
 
 }
@@ -12,8 +51,10 @@ class Database {
 
 
 
+$database = new Database();
+// $database->open_db_connection();
 
-}
+
 
 
 
