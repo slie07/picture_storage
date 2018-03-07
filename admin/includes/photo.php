033967bbbd570e533fbfe52fb7f69882883c0435
slie07 <liestevano@gmail.com>
2018-03-07 14:28:33 -0700

    revert the upload function

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -5,7 +5,7 @@ class Photo extends Db_object {
 	protected static $db_table = "photos";
     protected static $db_table_fields = array('photo_id','title', 'description', 'filename', 'type', 'size');
 
-	public $photo_id;
+	public $id;
 	public $title;
 	public $description;
 	public $filename;
